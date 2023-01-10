f1590d7587e49c0d1a3173e7440fa1de95632e42
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-10 17:01:09 +1000

    add task003

---
+++ task003/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+Console.Write($"Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+// O(n^2)
+
+Stopwatch sw = new();
+sw.Start();
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+     Console.Write($"{i * j,4}");  
+    }
+    Console.WriteLine();
+}
+
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
+
+// for (int i = 0; i < n; i++)
+// {
+//     matrix[i, n - i - 1] = (i + 1) * (n - i);
+//     matrix[i, i] = (i + 1) * (i + 1);
+// }
+
+sw.Reset();
+sw.Start();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+            matrix[i, j] = (i+1) * (j+1);
+            matrix[j, i] = matrix[i, j];
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+        Console.Write($"{matrix[i, j],4}");
+    Console.WriteLine();
+}
+
+// O(n^2 / 2 )
+
+sw.Stop();
+Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
