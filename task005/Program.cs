a6d6f52a09afcc5b9981a6600d0b0a5a2af7e689
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-12 11:28:11 +1000

    Добавили task005 и readme-файл описания проекта Сортировка выбором на C#

---
+++ task005/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3 };
+Console.WriteLine(string.Join("", arr));
+
+void sortIntArrayMinMax(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        for (int j = i+1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+        }
+    }
+}
+
+sortIntArrayMinMax(arr);
+Console.WriteLine(string.Join("", arr));
