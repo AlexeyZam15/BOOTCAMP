fbcf8b29517efcb3623349272d168477b2a224cc
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-12 12:21:54 +1000

    Полноценный проект сортировки и оформления проекта готов

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,8 +1,15 @@
 ﻿using static Sorting;
 using static Infrastructure;
 
-int[] array = CreateArray(10);
-Show(array);
+// int[] array = CreateArray(10);
+// Show(array);
+// SortSelection(array);
+// Show(array);
 
-SortSelection(array);
-Show(array);
+10.CreateArray(min:10, max: 30)
+    .Show(", ")
+    .SortSelection()
+    .Show(",");
+
+// double res = 2.Pow(10);
+// Console.WriteLine(res);
