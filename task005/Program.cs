b1fcd3ec908f229ba4f692f0e64f373e300425f5
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-12 11:40:31 +1000

    Добавлен простой пример реализации сортировки выбором C#

--- task005/Program.cs
+++ task005/Program.cs
@@ -1,22 +1,38 @@
-﻿
-int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3 };
+﻿int[] arr = { 1, 2, 3, 4, 5, 1, 2, 3 };
 Console.WriteLine(string.Join("", arr));
 
-void sortIntArrayMinMax(int[] arr)
+// void sortIntArrayMinMax(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         for (int j = i + 1; j < arr.Length; j++)
+//         {
+//             if (arr[j] < arr[i])
+//             {
+//                 int t = arr[j];
+//                 arr[j] = arr[i];
+//                 arr[i] = t;
+//             }
+//         }
+//     }
+// }
+
+void SortSelection(int[] collection)
 {
-    for (int i = 0; i < arr.Length - 1; i++)
+    int size = collection.Length;
+    for (int i = 0; i < size - 1; i++)
     {
-        for (int j = i+1; j < arr.Length; j++)
+        int pos = i;
+        for (int j = i+1; j < size; j++)
         {
-            if (arr[j] < arr[i])
-            {
-                int t = arr[j];
-                arr[j] = arr[i];
-                arr[i] = t;
-            }
+            if (collection[j] < collection[pos]) pos = j;
         }
+        int temp = collection[i];
+        collection[i] = collection[pos];
+        collection[pos] = temp;
     }
 }
 
-sortIntArrayMinMax(arr);
+// sortIntArrayMinMax(arr);
+SortSelection(arr);
 Console.WriteLine(string.Join("", arr));
