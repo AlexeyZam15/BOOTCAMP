3e13fb0530dba159f73c470698e8fdefe48c7400
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-30 16:39:18 +1000

    added task018 work with flask

---
+++ task018/main.py
@@ -0,0 +1,19 @@
+# Урок 23. Знакомство с Flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "<h1>Hello, world!</h1><br><a href='/index'>перейти на вторую страницу</a>"
+
+
+@app.route('/index/<x>/<y>')
+def index(x, y):
+    return f"Результат: {float(x) + float(y)}"
+
+
+if __name__ == '__main__':
+    app.run()
